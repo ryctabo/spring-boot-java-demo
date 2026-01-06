edda852cc5a77081bac6d462d060b70cf88945e7
Gustavo Pacheco <ryctabo@gmail.com>
2026-01-06 04:53:06 -0500

    chore: initial commit

---
+++ src/main/java/com/ryctabo/demo/SpringBootJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ryctabo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJavaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryctabo/demo/SpringBootJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryctabo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
